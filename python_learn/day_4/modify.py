c7c1761495496352df4dfb1f63550ab3d727c545
shuipingyang-14 <xy_1521675822@163.com>
2020-05-26 10:44:23 +0800

    修改文件内数据

--- python_learn/day_4/modify.py
+++ python_learn/day_4/modify.py
@@ -8,10 +8,9 @@
 """
 import os
 
-
 with open("file.txt", encoding='utf-8') as read_f, open(".file.txt.swap", 'w', encoding='utf-8') as write_f:
-    data = read_f.read()    # 全部读入内存,如果文件很大,会很卡
-    data = data.replace("alex", 'SS')   # 在内存中完成修改
+    data = read_f.read()  # 全部读入内存,如果文件很大,会很卡
+    data = data.replace("alex", 'SS')  # 在内存中完成修改
     write_f.write(data)  # 一次性写入新文件
 
 os.remove("file.txt")
