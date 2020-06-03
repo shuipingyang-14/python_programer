cce971af7cae0331bcbe219238f1ffdb722bf01a
shuipingyang-14 <xy_1521675822@163.com>
2020-06-03 18:02:22 +0800

    冒泡排序

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -7,22 +7,30 @@
 
 
 def bubble_sort(lists):
-	"""
-	冒泡排序
-	:param lists: 待排序数组
-	:return: 排序完成的数组
-	"""
-	count = 0
-	for i in range(len(lists)-1):
-		for j in range(len(lists)-i-1):
-			if lists[j] > lists[j+1]:
-				lists[j],lists[j+1] = lists[j+1], lists[j]
-			count += 1
-	print(count)
-	return lists
+    """
+    冒泡排序
+    :param lists: 待排序数组
+    :return: 排序完成的数组
+    """
+    count = 0
+    for i in range(len(lists) - 1):
+        for j in range(len(lists) - i - 1):
+            if lists[j] > lists[j + 1]:
+                lists[j], lists[j + 1] = lists[j + 1], lists[j]
+            count += 1
+    print(count)
+    return lists
+
+
+def bubble_sort_1(lists):
+    for i in range(len(lists) - 1, 0, -1):
+        for j in range(i):
+            if lists[j] > lists[j + 1]:
+                lists[j], lists[j + 1] = lists[j + 1], lists[j]
+    return lists
 
 
 if __name__ == "__main__":
-	list1 = [38, 65, 97, 76, 13, 27, 49]
-	list2 = bubble_sort(list1)
-	print(list2)
+    list1 = [38, 65, 97, 76, 13, 27, 49]
+    list2 = bubble_sort_1(list1)
+    print(list2)
