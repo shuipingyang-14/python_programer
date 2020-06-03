73162bbe5c8caa1b310d4e28256785a8cd6cecc5
shuipingyang-14 <xy_1521675822@163.com>
2020-06-03 18:02:38 +0800

    选择排序

--- sort/select_sort.py
+++ sort/select_sort.py
@@ -7,25 +7,25 @@
 
 
 def select_sort(lists):
-	"""
-	选择排序
-	:param lists: 待排序树组
-	:return: 排序完成数组
-	"""
-	count = 0
-	for i_index in range(0, len(lists)):
-		min = i_index
-		for j_index in range(i_index+1, len(lists)):
-			# 找出最小值
-			if lists[min] > lists[j_index]:
-				min = j_index
-			count += 1
-		lists[min], lists[i_index] = lists[i_index], lists[min]
-	print(count)
-	return lists
+    """
+    选择排序
+    :param lists: 待排序树组
+    :return: 排序完成数组
+    """
+    count = 0
+    for i in range(0, len(lists)):
+        min = i
+        for j in range(i + 1, len(lists)):
+            # 找出最小值
+            if lists[min] > lists[j]:
+                min = j
+            count += 1
+        lists[min], lists[i] = lists[i], lists[min]
+    print(count)
+    return lists
 
 
 if __name__ == "__main__":
-	list1 = [38, 65, 97, 76, 13, 27, 49]
-	list2 = select_sort(list1)
-	print(list2)
+    list1 = [38, 65, 97, 76, 13, 27, 49]
+    list2 = select_sort(list1)
+    print(list2)
