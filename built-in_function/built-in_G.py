da1a206c7ee73847733962ce3d59f982ba4ee12c
shuipingyang-14 <xy_1521675822@163.com>
2020-06-04 18:13:28 +0800

    G类内置函数

--- built-in_function/built-in_G.py
+++ built-in_function/built-in_G.py
@@ -7,19 +7,31 @@
 @ version:  python 3.8.3
 """
 
+
 def getattr_test():
     """
     返回对象属性值
     :return:
     """
+
     class A(object):
         bar = 1
 
     a = A()
-    print(getattr(a, 'bar'))   # 获取属性值bar值
+    print(getattr(a, 'bar'))  # 获取属性值bar值
     # print(getattr(a, 'bar2'))  # 属性 bar2 不存在，触发异常
-    print(getattr(a, 'bar2', 3))  #  属性 bar2 不存在，设置默认值
+    print(getattr(a, 'bar2', 3))  # 属性 bar2 不存在，设置默认值
+
+
+def global_test():
+    """
+    以字典类型返回当前位置全部全局变量
+    :return:
+    """
+    a = 'hello'
+    print(globals())  # globals 函数返回一个全局变量的字典，包括所有导入的变量
 
 
 if __name__ == '__main__':
     getattr_test()
+    global_test()
