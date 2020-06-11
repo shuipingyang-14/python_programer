f721d2a7dce30ee421349da8bd738a3be59e7dc1
shuipingyang-14 <xy_1521675822@163.com>
2020-06-11 13:15:31 +0800

    I类内置函数

--- built-in_function/built-in_I.py
+++ built-in_function/built-in_I.py
@@ -7,32 +7,35 @@
 @ version:  python 3.8.3
 """
 
+
 def id_test():
     """
     返回对象唯一标识符
     :return:
     """
     a = "hello"
-    print(id(a))   # 2017188377968
+    print(id(a))  # 2017188377968
 
     b = 1
     print(id(b))  # 140718175295136
 
-    c = [1,2,3]
-    print(id(c))   # 2017188389120
+    c = [1, 2, 3]
+    print(id(c))  # 2017188389120
+
 
 def input_test():
     """
     输入数据，返回字符串
     :return:
     """
-    a = input("input:")   # 输入整数
+    a = input("input:")  # 输入整数
     print(a)
     print(type(a))
 
-    b = input("input:")   # 输入字符串
+    b = input("input:")  # 输入字符串
     print(b)
     print(type(b))
+
 
 def int_test():
     """
@@ -43,8 +46,9 @@ def int_test():
     print(int(3))
     print(int(3.6))
     print(int('10', 2))
-    print(int('0xa',16))
-    print(int('12',16))
+    print(int('0xa', 16))
+    print(int('12', 16))
+
 
 def isinstance_test():
     """
@@ -52,14 +56,14 @@ def isinstance_test():
     :return:
     """
     a = 2
-    print(isinstance(a, int))   # True
-    print(isinstance (a,str))   # False
-    print(isinstance (a,(str,int,list)))   # True
+    print(isinstance(a, int))  # True
+    print(isinstance(a, str))  # False
+    print(isinstance(a, (str, int, list)))  # True
 
-    l1 = [1,2,3]
-    print(isinstance(l1, int))   # False
-    print(isinstance(l1,str))   # False
-    print(isinstance(l1,(str,int,list)))   # True
+    l1 = [1, 2, 3]
+    print(isinstance(l1, int))  # False
+    print(isinstance(l1, str))  # False
+    print(isinstance(l1, (str, int, list)))  # True
 
     class A:
         pass
@@ -67,10 +71,10 @@ def isinstance_test():
     class B(A):
         pass
 
-    print(isinstance(A(), A))   # True
-    print(type(A()) == A)   # True
-    print(isinstance(B(), A))   # True
-    print(type(B()) == A)   # False
+    print(isinstance(A(), A))  # True
+    print(type(A()) == A)  # True
+    print(isinstance(B(), A))  # True
+    print(type(B()) == A)  # False
 
 
 def issubclass_test():
@@ -87,9 +91,21 @@ def issubclass_test():
 
     print(issubclass(B, A))  # True
 
+
+def iter_test():
+    """
+    生成迭代器
+    :return:
+    """
+    l1 = [1, 2, 3]
+    for i in iter(l1):
+        print(i)
+
+
 if __name__ == '__main__':
     id_test()
     # input_test()
     int_test()
     isinstance_test()
     issubclass_test()
+    iter_test()
