ac62f579ac46d0b386539c1da1f1962b59f40324
shuipingyang-14 <xy_1521675822@163.com>
2020-06-23 10:43:19 +0800

    __import__使用

---
+++ built-in_function/a.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8
+"""
+@ author:   ysp
+@ time：    2020/6/23 10:16
+@ file:     a.py
+@ IDE:      PyCharm
+@ version:  python 3.8.3
+"""
+
+import os
+
+print('在 a.py 文件中 %s' % id(os))
---
+++ built-in_function/import_test.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8
+"""
+@ author:   ysp
+@ time：    2020/6/23 10:15
+@ file:     import_test.py
+@ IDE:      PyCharm
+@ version:  python 3.8.3
+"""
+
+
+import sys
+__import__('class_method')  # 导入 a.py 模块
