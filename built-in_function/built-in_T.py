f4eca8dce234d019c7f292bb40df9d458249976f
shuipingyang-14 <xy_1521675822@163.com>
2020-06-23 10:43:32 +0800

    T类内置函数

--- built-in_function/built-in_T.py
+++ built-in_function/built-in_T.py
@@ -7,6 +7,7 @@
 @ version:  python 3.8.3
 """
 
+
 def tuple_test():
     """
     将可迭代对象转换为元组
@@ -15,10 +16,10 @@ def tuple_test():
     res = tuple([1, 2, 3, 4])
     print(res)
 
-    res = tuple({1:2,3:4}) # 针对字典 会返回字典的key组成的tuple
+    res = tuple({1: 2, 3: 4})  # 针对字典 会返回字典的key组成的tuple
     print(res)
 
-    res = tuple((1,2,3,4))    #元组会返回元组自身
+    res = tuple((1, 2, 3, 4))  # 元组会返回元组自身
     print(res)
 
     aList = [123, 'xyz', 'zara', 'abc']
@@ -26,5 +27,43 @@ def tuple_test():
 
     print("Tuple elements : ", aTuple)
 
+
+def type_test():
+    """
+    返回对象类型
+    :return:
+    """
+    # 一个参数
+    print(type(1))
+    print(type('hello'))
+    print([1])
+    print(type({'1': 1}))
+    print(type((1,)))
+    print(type(1) == int)
+
+    # 三个参数
+    class X(object):
+        a = 1
+
+    X = type('X', (object,), dict(a=1))
+    print(X)  # <class '__main__.X'>
+
+    # type和isinstance区别：
+    #type()不会认为子类是一种父类类型，不考虑继承关系
+
+    #isinstance()会认为子类是一种父类类型，考虑继承关系
+    class A:
+        pass
+
+    class B(A):
+        pass
+
+    print(isinstance(A(), A)) # true
+    print(type(A()) == A) # true
+    print(isinstance(B(), A)) # true
+    print(type(B()) == A) # false
+
+
 if __name__ == '__main__':
     tuple_test()
+    type_test()
