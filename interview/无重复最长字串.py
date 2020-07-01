0fc893985680a180f8f1ab71b34809beaa2ba967
shuipingyang-14 <xy_1521675822@163.com>
2020-07-01 17:12:12 +0800

    无重复最长字串

--- interview/无重复最长字串.py
+++ interview/无重复最长字串.py
@@ -36,7 +36,7 @@ def lengthOfLongestSubstring(s: str):
 
 
 if __name__ == '__main__':
-    one_str_list = ['120135435', 'abdfkjkgdok', '123456780423349']
+    one_str_list = ['1201355435', 'abdfkjkgdok', '123456780423349']
     for one_str in one_str_list:
         res = lengthOfLongestSubstring(one_str)
         print('{0}最长非重复子串长度为：{1}'.format(one_str, res))
