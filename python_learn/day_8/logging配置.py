2d0a4ab0dcd387f6cc115b207572e785fd7a0283
shuipingyang-14 <xy_1521675822@163.com>
2020-07-06 19:30:31 +0800

    logging配置

--- python_learn/day_8/logging配置.py
+++ python_learn/day_8/logging配置.py
@@ -12,7 +12,7 @@ import logging.config
 
 # 定义日志输出格式
 standard_format = '[%(asctime)s] [%(threadName)s:%(thread)d] [task_id:%(name)s] [%(filename)s:%(lineno)d]' \
-                  '[%(levelname)s] [%(message)s]' #其中name为getlogger指定的名字
+                  '[%(levelname)s] [%(message)s]'  # 其中name为getlogger指定的名字
 simple_format = '[%(levelname)s] [%(asctime)s] [%(filename)s:%(lineno)d] %(message)s'
 id_simple_format = '[%(levelname)s][%(asctime)s] %(message)s'
 
@@ -41,25 +41,25 @@ LOGGING_DIC = {
     },
     'filters': {},
     'handlers': {
-        #打印到终端的日志
+        # 打印到终端的日志
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',  # 打印到屏幕
             'formatter': 'simple'
         },
-        #打印到文件的日志,收集info及以上的日志
+        # 打印到文件的日志,收集info及以上的日志
         'default': {
             'level': 'DEBUG',
             'class': 'logging.handlers.RotatingFileHandler',  # 保存到文件
             'formatter': 'standard',
             'filename': logfile_path,  # 日志文件
-            'maxBytes': 1024*1024*5,  # 日志大小 5M
+            'maxBytes': 1024 * 1024 * 5,  # 日志大小 5M
             'backupCount': 5,
             'encoding': 'utf-8',  # 日志文件的编码，再也不用担心中文log乱码了
         },
     },
     'loggers': {
-        #logging.getLogger(__name__)拿到的logger配置
+        # logging.getLogger(__name__)拿到的logger配置
         '': {
             'handlers': ['default', 'console'],  # 这里把上面定义的两个handler都加上，即log数据既写入文件又打印到屏幕
             'level': 'DEBUG',
@@ -67,6 +67,7 @@ LOGGING_DIC = {
         },
     },
 }
+
 
 def load_my_logging_cfg():
     logging.config.dictConfig(LOGGING_DIC)  # 导入上面定义的logging配置
@@ -77,6 +78,7 @@ def load_my_logging_cfg():
     logger.warning('logger warning message')
     logger.error('logger error message')
     logger.critical('logger critical message')
+
 
 if __name__ == '__main__':
     load_my_logging_cfg()
